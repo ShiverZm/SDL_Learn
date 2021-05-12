670db14aebef75e5b4f1ea920556def6951ae170
Shiver <Shiver@wangxutech.com>
2021-05-12 11:44:45 +0800

    add SDL2_image-2.0.4

--- build/cmake_build_vs2019.bat
+++ build/cmake_build_vs2019.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PATH=%PATH%;../build/cmake/windows/bin;
+set PATH=%PATH%;../build/cmake_3.20/windows/bin;
 cd ..
-mkdir tmp
+mkdir tmp_2019
 cd tmp
 cmake .. -G"Visual Studio 16 2019" -A Win32 -DCMAKE_GENERATOR_TOOLSET=v142 -DCMAKE_CONFIGURATION_TYPES=Debug;Release
 pause
